b5cb9ef458794877ac98fe8313f8321b9a9f1a73
Chaitanya Popali <chintu2411@gmail.com>
2025-07-22 14:47:05 +0530

    my App

---
+++ custom_button/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'POS Amount Based Qunatity Calculator',
+    'version': '18.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Enter amount in ₹ and auto-calculate product quantity in POS.',
+    'author': 'Spopli Web Development & Services',
+    'company': 'Spopli Web Development & Services',
+    'maintainer': 'Spopli Web Development & Services',
+    'website': 'https://spopli.com', 
+    'depends': ['base', 'point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'custom_button/static/src/js/custom_button.js',
+            'custom_button/static/src/xml/custom_button.xml',
+            'custom_button/static/src/css/custom_button.css',
+        ],
+    },
+    'images': [
+        'static/description/icon.png',
+        'static/description/banner.png',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
