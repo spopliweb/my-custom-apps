d9384fad7c106d41192366eb69780384f55d3b02
Anmol Patil <130757102+anmol6213@users.noreply.github.com>
2026-03-07 11:46:51 +0530

    Update __manifest__.py

--- custom_button/__manifest__.py
+++ custom_button/__manifest__.py
@@ -7,7 +7,7 @@
     'company': 'Spopli Web Development & Services',
     'maintainer': 'Spopli Web Development & Services',
     'website': 'https://spopli.com', 
-    'depends': ['base', 'point_of_sale'],
+    'depends': ['point_of_sale'],
     'assets': {
         'point_of_sale._assets_pos': [
             'custom_button/static/src/js/custom_button.js',
